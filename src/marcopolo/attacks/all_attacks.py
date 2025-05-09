56d1d0cc1bc25910742fe97231d8693ae3c2a2e5
Ari Brown <yabrown@princeton.edu>
2025-05-09 03:04:19 -0400

    added feature-- can now run custom attack pairs

--- src/marcopolo/attacks/all_attacks.py
+++ src/marcopolo/attacks/all_attacks.py
@@ -20,6 +20,7 @@ from marcopolo.utils.results_writer import initialize_result_files, reset_state,
 from marcopolo.utils.logs_writer import clear_log_files
 
 
+   
 def all_attacks(force_restart: bool = False, clear_logs: bool = False):
   """
   Executes a series of attacks between pairs of nodes using different certificate authorities.
@@ -91,13 +92,53 @@ def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Run Marco-Polo attacks')
     parser.add_argument('--force-restart', action='store_true', help='Force restart the test from the beginning')
     parser.add_argument('--clear-logs', action='store_true', help='Clear all log files before starting')
+    parser.add_argument(
+        '--custom-attacks-file',
+        type=argparse.FileType('r'),
+        help="Path to a file containing node pairs, one per line, formatted as 'nodeA,nodeB'"
+    )
     return parser.parse_args()
+
 
 def main() -> None:
     # Basically only exists so we can call it from command line with arguments as a package (e.g. marcopolo.attacks.all_attacks.main --force-restart --clear-logs)
     args = parse_arguments()
-    all_attacks(force_restart=args.force_restart, clear_logs=args.clear_logs)
+    if args.custom_attacks_file:
+        pairs = [tuple(line.strip().split(",")) for line in args.custom_attacks_file if line.strip()]
+        run_custom_attacks(pairs)
+    else:
+        all_attacks(force_restart=args.force_restart, clear_logs=args.clear_logs)
     pathfinder(["-w"]) # withdraw all announcements
-   
+
+
+def run_custom_attacks(pairs: list[tuple[str, str]]):
+  general_logger.debug("Starting custom attacks for the following pairs:")
+  for pair in pairs:
+      general_logger.debug(f"  {pair[0]}, {pair[1]}")
+
+  # Load info needed for game (loaders will raise error if anything's wrong, which should cause a quit)
+  config = load_config()
+  ca_list = config.certificate_authorities
+  nodes = config.nodes
+  
+  for node_a_name, node_b_name in pairs:
+      node_a = next((node for node in nodes if node.name == node_a_name), None)
+      node_b = next((node for node in nodes if node.name == node_b_name), None)
+      if not node_a or not node_b:
+          general_logger.error(f"Could not find node objects for {node_a_name} and/or {node_b_name}")
+          continue
+      print(f"Running custom attack between {node_a} and {node_b}")
+      #run attack and record results
+      round = Round(
+          cas=ca_list,
+          bgp_prefix="66.180.191.0/24",
+          bgp_propagation_delay=config.bgp_propagation_delay,
+          node_a=node_a,
+          node_b=node_b
+      )
+      round_data = round.execute()
+      record_results(round_data)
+
+
 if __name__=="__main__":
     main()
